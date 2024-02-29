5efacf8dc96669641d42e2a4876ea3165da801ac
Eric Kapalka <eric.kapalka@montana.edu>
2024-02-29 14:23:08 -0700

    Added looping logic to main_loop

--- browsercontrol/banner_driver.py
+++ browsercontrol/banner_driver.py
@@ -11,7 +11,6 @@ class BannerDriver:
     This class is used to control the Banner web page.
     """
     def __init__(self):
-        self.processed_ids = set()
         self.batch_ids = set()
         self.driver = webdriver.Chrome()
         self.await_login()
@@ -25,22 +24,18 @@ class BannerDriver:
         self.driver.get("https://prodbanner.montana.edu/applicationNavigator/seamless")
         WebDriverWait(self.driver, 300).until(EC.title_is("Application Navigator"))
         print("Banner Login Successful")
-        self.driver.get("https://prodbanner.montana.edu/BannerAdmin?form=SRIPREL&vpdi_code="
-                        "BZ&appnav_vpdi_code=BZ&ban_args=&ban_mode=xe")
 
     def main_loop(self):
         """
         The main loop of the program
         :return: None
         """
-        print("Main Loop")
-        filter_again(self.driver)
-        time.sleep(100)
-        # self.batch_ids = get_prospect_ids(self.driver)
-        # print(self.batch_ids)
-
-        # for prospect_id in self.batch_ids:
-        #     ...
-        # if self.batch_ids.issubset(self.processed_ids):
-        #     print("No new prospects found")
-        #     return
+        while True:
+            filter_again(self.driver)
+            self.batch_ids = get_prospect_ids(self.driver)
+            if not self.batch_ids:
+                print("No new prospects found")
+                return
+            for prospect_id in self.batch_ids:
+                print(prospect_id)
+                # TODO: navigate to it and do stuff
