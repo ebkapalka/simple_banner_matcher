789bafde691fd4344cc8fd88e0ca1c8b9423268f
Eric Kapalka <eric.kapalka@montana.edu>
2024-02-29 12:54:40 -0700

    Fixed wait_for_verifier_load function so it actually works. Renamed configure_verifier to filter_again and added all logic to make it function.  Will have to test how it works when the filters are collapsed.  Added get_prospect_ids function

--- browsercontrol/sriprel_navigator.py
+++ browsercontrol/sriprel_navigator.py
@@ -5,41 +5,88 @@ the verifier page (Electronic Prospect Inquiry; SRIPREL).
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
+import sys
 
 
 def wait_for_verifier_load(driver: webdriver, timeout=300):
-    selector_workspace_title = ("body > nav.workspace-header.navbar.navbar-default.navbar-expand-lg.navbar-fixed-top > "
-                                "div > div.navbar-header > ul > li:nth-child(3) > h2")
-    WebDriverWait(driver, timeout).until(
-        lambda d: "SRIPREL" in d.find_element(By.CSS_SELECTOR, selector_workspace_title).text
-    )
+    """
+    Wait for the verifier page to load, checking multiple elements for specific text.
+    :param driver: webdriver
+    :param timeout: time to wait for the page to load
+    :return: None
+    """
+    def any_element_contains_text(driver) -> bool:
+        """
+        Check if any of the elements contain the text "SRIPREL"
+        :return: whether any of the elements contain the text "SRIPREL"
+        """
+        elements = driver.find_elements(By.CLASS_NAME, "workspace-title")
+        return any("SRIPREL" in element.text for element in elements)
+
+    print("Waiting for verifier page to load...")
+    WebDriverWait(driver, timeout).until(any_element_contains_text)
+    print("Verifier page loaded")
 
 
-def configure_verifier(driver: webdriver, timeout=300):
+def filter_again(driver: webdriver, timeout=300):
     """
     Set the configuration for the verifier page
+    :param driver: webdriver
+    :param timeout: time to wait for the page to load
     :return: None
     """
     wait_for_verifier_load(driver)
-    selector_match_status = "#frames16_ac"
-    selector_go_button = ("#frames3_content > div.filterForm.ui-widget.ui-filterpanel.ui-basic-filter-mode > div > "
-                          "div.legendDown > span > button.primary-button.ui-buttonGo")
-    input_match_status = WebDriverWait(driver, timeout).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, selector_match_status)))
-    driver.execute_script("arguments[0].value = arguments[1];",
-                          input_match_status, "Suspense")
-    button_go = WebDriverWait(driver, timeout).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, selector_go_button)))
-    button_go.click()
-    time.sleep(100)
+    selector_filter_elements = (By.CLASS_NAME, 'middleDivRow')
+    selector_text_input = (By.XPATH, ".//input")
+    selector_button_go = (By.CLASS_NAME, "ui-buttonGo")
+    btn = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable(selector_button_go))
+    elems = driver.find_elements(*selector_filter_elements)
+    for index, elem in enumerate(elems):
+        try:
+            elem_labels = elem.find_elements(By.XPATH, ".//label")
+            elem_label_text = elem_labels[2].text
+        except NoSuchElementException:
+            continue
+        except IndexError:
+            continue
+
+        if "Match Status" in elem_label_text:
+            input_box = elem.find_elements(
+                *selector_text_input)[1]
+            input_box.click()
+            cmd = "arguments[0].value = 'Suspense';"
+            driver.execute_script(cmd, input_box)
+            btn.click()
+            break
 
 
-def get_rows(driver: webdriver, timeout=300) -> list[WebElement]:
+def get_prospect_ids(driver: webdriver, timeout=300) -> list[str]:
+    """
+    Fetch the rows from the verifier page
+    :param driver: webdriver
+    :param timeout: time to wait for the page to load
+    :return: list of WebElements
+    """
+    prospect_ids = []
     wait_for_verifier_load(driver)
+    print("Fetching Prospect IDs from verifier page...")
+    time.sleep(10)
+    rows = driver.find_elements(By.XPATH, '//div[@onmousedown="Frames.DataGrid.selection(this);"]')
+    print(len(rows))
+    for row in rows:
+        try:
+            child_div = row.find_element(By.XPATH, ".//div[1]/div[1]")
+        except:
+            child_div = row.find_element(By.XPATH, ".//div[1]/input[1]")
+        prospect_id = child_div.text
+        print(prospect_id)
+        prospect_ids.append(prospect_id)
 
 
-def next_page(driver: webdriver, timeout=300):
-    wait_for_verifier_load(driver)
+def select_row_by_prospect_id(driver: webdriver, prospect_id: str, timeout=300) -> WebElement:
+    ...
