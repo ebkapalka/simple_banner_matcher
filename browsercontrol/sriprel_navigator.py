01e6d1591fb28bc5d134aa4bbc36b14fbbc810b3
Eric Kapalka <eric.kapalka@montana.edu>
2024-02-29 17:03:40 -0700

    Fleshed out select_by_prospect_id function, added select_and_nav function too to go to the chosen prospect_id's GOAMTCH page

--- browsercontrol/sriprel_navigator.py
+++ browsercontrol/sriprel_navigator.py
@@ -1,7 +1,4 @@
-"""
-The table navigator expects that the curtrently active page is
-the verifier page (Electronic Prospect Inquiry; SRIPREL).
-"""
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
@@ -19,12 +16,12 @@ def wait_for_verifier_load(driver: webdriver, timeout=300):
     :param timeout: time to wait for the page to load
     :return: None
     """
-    def any_element_contains_text(driver) -> bool:
+    def any_element_contains_text(dr: webdriver) -> bool:
         """
         Check if any of the elements contain the text "SRIPREL"
         :return: whether any of the elements contain the text "SRIPREL"
         """
-        elements = driver.find_elements(By.CLASS_NAME, "workspace-title")
+        elements = dr.find_elements(By.CLASS_NAME, "workspace-title")
         return any("SRIPREL" in element.text for element in elements)
 
     print("Waiting for verifier page to load...")
@@ -75,14 +72,14 @@ def get_prospect_ids(driver: webdriver, timeout=10) -> list[str]:
     :param timeout: time to wait for the page to load
     :return: list of WebElements
     """
-    def await_multiple_elems(driver: webdriver):
+    def await_multiple_elems(dr: webdriver):
         """
         Used to check for multiple matching elements
         :param driver: webdriver
         :return:
         """
-        elems = driver.find_elements(By.XPATH, '//div[@onmousedown="Frames.'
-                                               'DataGrid.selection(this);"]')
+        elems = dr.find_elements(By.XPATH, '//div[@onmousedown="Frames.'
+                                           'DataGrid.selection(this);"]')
         if len(elems) < 2:
             return False
         return elems
@@ -100,5 +97,56 @@ def get_prospect_ids(driver: webdriver, timeout=10) -> list[str]:
     return prospect_ids
 
 
-def select_row_by_prospect_id(driver: webdriver, prospect_id: str, timeout=300) -> WebElement:
-    ...
+def select_by_prospect_id(driver: webdriver, prospect_id: str) -> WebElement | None:
+    """
+    Select a row by the prospect id
+    :param driver: webdriver
+    :param prospect_id: id to search for
+    :return: element if found, None if not found
+    """
+    wait_for_verifier_load(driver)
+    elems = driver.find_elements(By.XPATH, '//div[@onmousedown="Frames.'
+                                           'DataGrid.selection(this);"]')
+    for elem in elems:
+        try:
+            child_div = elem.find_element(By.XPATH, ".//div[1]/div[1]")
+        except:
+            child_div = elem.find_element(By.XPATH, ".//div[1]/input[1]")
+        elem_id = child_div.text
+        if elem_id == prospect_id:
+            return elem
+    return None
+
+
+def select_and_nav(driver: webdriver, actions: ActionChains, elem: WebElement, timeout=30):
+    def await_menu_related(dr: webdriver):
+        """
+        Used to check for the menu related element
+        :param driver: webdriver
+        :return:
+        """
+        menu_elem = dr.find_element(By.ID, 'menu-related')
+        if "menu-open" not in menu_elem.get_attribute("class"):
+            actions.key_down(Keys.ALT).key_down(Keys.SHIFT)
+            actions.send_keys('r')
+            actions.key_up(Keys.SHIFT).key_up(Keys.ALT)
+            actions.perform()
+            time.sleep(0.1)
+            return False
+        return True
+
+    while elem.get_attribute("aria-selected") != "true":
+        time.sleep(.1)
+        elem.click()
+
+    WebDriverWait(driver, timeout).until(await_menu_related)
+    driver.find_element("xpath", '//*[@data-action="GOTO_MATCH"]').click()
+    continue_btn = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@data-member="SPAIDEN_ASSOCIATE_PERSON_BTN"]')))
+    continue_btn.click()
+    continue_btn = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@data-member="EXECUTE_BTN"]')))
+    continue_btn.click()
+    continue_btn = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@data-member="CHECK_BTN"]')))
+    continue_btn.click()
