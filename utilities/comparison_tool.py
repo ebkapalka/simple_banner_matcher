c99c0f92c46ccface6e85aeabd0a2b35ac521f0c
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-03 23:03:31 -0700

    Added compare_prospects and _compare_dicts functions to determine best match

---
+++ utilities/comparison_tool.py
@@ -0,0 +1,42 @@
+from fuzzywuzzy import fuzz
+import re
+
+PATTERN = re.compile(r'\D')
+
+
+def compare_prospects(prospect: dict[str, str], potential_matches: dict[str, dict]) -> str:
+    """
+    Compare the prospect to the potential matches
+    :param prospect: prospect attributes in a dictionary
+    :param potential_matches: dictionary of potential matches
+    :return:
+    """
+    normalized_prospect = {
+            "name": f"{prospect["last name"]}, {prospect["first name"]} {prospect["middle name"]}".strip(),
+            "birthday": '/'.join([prospect['mm'], prospect['dd'], prospect['yyyy']]).strip().replace('//', ''),
+            "address": f"{prospect['street 1']} {prospect['city']} {prospect['state']} {prospect['zipcode'][:5]}".strip(),
+            "phone": PATTERN.sub('', f"{prospect['phone area']} {prospect['phone number']}"),  # this might need [-10:
+            "email": prospect['email'].strip(),
+            "gender": '',
+    }
+    for match in potential_matches:
+        scores = _compare_dicts(normalized_prospect, potential_matches[match])
+        if 100 in [scores['phone'], scores['email'], scores['address']]:
+            return match
+    return ''
+
+
+def _compare_dicts(dict1: dict[str, str], dict2: dict[str, str]) -> dict[str, int]:
+    """
+    Compare two dictionaries using the fuzzywuzzy library
+    :param dict1: dictionary of strings
+    :param dict2: dictionary of strings with the same keys as dict1
+    :return: dictionary of scores
+    """
+    scores = {}
+    for key in dict1:
+        if dict1[key].strip() and dict2[key].strip():
+            scores[key] = fuzz.ratio(dict1[key], dict2[key])
+        else:
+            scores[key] = 0
+    return scores
